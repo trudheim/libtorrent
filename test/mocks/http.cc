df644b95c2942ce5bea2393ddb1d992fdb82bcd8
rakshasa <sundell.software@gmail.com>
2021-08-04 23:12:47 +0900

    Fixed http_getter.

--- test/mocks/http.cc
+++ test/mocks/http.cc
@@ -4,6 +4,25 @@
 
 namespace mocks {
 
+http_getter::http_getter() :
+  m_flags(0),
+  m_destroyed_status(nullptr) {}
+
+http_getter::~http_getter() {
+  if (m_destroyed_status != nullptr)
+    *m_destroyed_status = true;
+}
+
+void
+http_getter::start() {
+  m_flags |= flag_active;
+}
+
+void
+http_getter::close() {
+  m_flags &= ~flag_active;
+}
+
 bool
 http_getter::trigger_signal_done() {
   if (!(m_flags & flag_active))
@@ -11,6 +30,7 @@ http_getter::trigger_signal_done() {
 
   m_flags &= ~flag_active;
   trigger_done();
+
   return true;
 }
 
@@ -21,6 +41,7 @@ http_getter::trigger_signal_failed() {
 
   m_flags &= ~flag_active;
   trigger_failed("We Fail.");
+
   return true;
 }
 
--- test/mocks/http.h
+++ test/mocks/http.h
@@ -2,9 +2,6 @@
 
 namespace mocks {
 
-// TODO: Refactor test_tracker_list.
-// TODO: Refacotr http stream to use shared_ptr.
-
 class http_getter : public torrent::Http {
 public:
   static const int flag_active = 0x1;
@@ -12,35 +9,21 @@ public:
   http_getter();
   ~http_getter() override;
 
-  void start() override { m_test_flags |= flag_active; }
-  void close() override { m_test_flags &= ~flag_active; }
+  void start() override;
+  void close() override;
 
   bool trigger_signal_done();
   bool trigger_signal_failed();
 
-  void set_destroyed_status(bool* status) { m_destroyed_status = status; }
+  void set_destroyed_status(bool* status);
 
 private:
-  int   m_test_flags;
+  int   m_flags;
   bool* m_destroyed_status;
 };
 
 inline http_getter* create_http_getter() { return new http_getter; }
 
-inline http_getter::http_getter() :
-  m_test_flags(0),
-  m_destroyed_status(nullptr) {}
-
-inline http_getter::~http_getter() {
-  if (m_destroyed_status != nullptr)
-    *m_destroyed_status = true;
-}
-
-// TODO: Move.
-// uint32_t controller_receive_success(torrent::Tracker*, torrent::AddressList*) { return 0; }
-// void     controller_receive_failure(torrent::Tracker*, const std::string&) {}
-// void     controller_receive_scrape(torrent::Tracker*) {}
-// void     controller_receive_tracker_enabled(torrent::Tracker*) {}
-// void     controller_receive_tracker_disabled(torrent::Tracker*) {}
+inline void http_getter::set_destroyed_status(bool* status) { m_destroyed_status = status; }
 
 }
